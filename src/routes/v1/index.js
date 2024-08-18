3a22e368f812f8ad6087b166f00c964003ea3569
Toan <nguyentoan04.0003@email.com>
2024-08-18 12:55:52 +0700

    remove Console.log()

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,12 +1,9 @@
 import express from 'express'
-import { StatusCodes } from 'http-status-codes'
 import { BoardRoute } from './dashBoardRoute'
 import { ColumnRoute } from './ColumnRoute'
 import { CardRoute } from './CardRoute'
 const Router = express.Router()
-Router.get('/status', (req, res) => {
-  res.status(StatusCodes.OK).send('ok')
-})
+
 Router.use('/dashboards', BoardRoute)
 Router.use('/columns', ColumnRoute)
 Router.use('/cards', CardRoute)
